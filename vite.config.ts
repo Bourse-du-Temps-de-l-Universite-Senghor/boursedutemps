48bc891f8636216da720cbc946f834ece733e12c
Palechonet <jeanbernardpierrelouis@gmail.com>
2026-02-19 01:25:14 +0200

    Mise à jour index.tsx et index.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ⚠️ Remplace "TonCompteGitHub" et "nom-du-depot" par ton vrai utilisateur et dépôt
+// ⚡ Important : remplace "boursedutemps" par le nom exact de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/boursedutemps/', 
+  base: '/boursedutemps/'
 });
 
