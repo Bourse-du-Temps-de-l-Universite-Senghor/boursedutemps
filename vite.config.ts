31fc588895a850a9d53255bc349b4273d929ab76
Palechonet <jeanbernardpierrelouis@gmail.com>
2026-02-19 12:10:30 +0200

    Fix vite config + vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// ⚡ Important : remplace "boursedutemps" par le nom exact de ton repo GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/boursedutemps/'
-});
+  base: './', // important pour éviter la page blanche
+})
 
